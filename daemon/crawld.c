ae6f5291668c0819a7cfa3948aee42438f275a9d
Mo McRoberts <mo.mcroberts@bbc.co.uk>
2014-10-04 23:01:13 +0000

    Perform proper command-line option handling.

--- daemon/crawld.c
+++ daemon/crawld.c
@@ -25,31 +25,24 @@
 
 #include "p_crawld.h"
 
+static int process_args(int argc, char **argv);
 static int config_defaults(void);
+static void usage(void);
+
+static const char *short_program_name = "crawler";
 
 int
 main(int argc, char **argv)
 {
-	char *t;
-	
-	t = strrchr(argv[0], '/');
-	if(t)
-	{
-		t++;
-	}
-	else
-	{
-		t = argv[0];
-	}
-	log_set_ident(t);
 	log_set_stderr(1);
 	log_set_facility(LOG_DAEMON);
 	log_set_level(LOG_NOTICE);
 	config_init(config_defaults);
-	if(argc > 1)
+	if(process_args(argc, argv))
 	{
-		config_set("global:configFile", argv[1]);
-	}	
+		exit(EXIT_FAILURE);
+	}
+	log_set_ident(short_program_name);
 	if(config_load(NULL))
 	{
 		return 1;
@@ -94,5 +87,76 @@ config_defaults(void)
 	/* Log to stderr initially, until daemonized */
 	config_set_default("log:stderr", "1");
 	config_set_default("global:configFile", SYSCONFDIR "/crawl.conf");
+	config_set_default("instance:crawler", "1");
+	config_set_default("instance:crawlercount", "1");
+	config_set_default("instance:cache", "1");
+	config_set_default("instance:cachecount", "1");
+	config_set_default("instance:threadcount", "1");	
+	config_set_default("crawl:pidfile", LOCALSTATEDIR "/run/crawld.pid");
+	config_set_default("crawl:queue", "db");
+	config_set_default("crawl:processor", "rdf");
+	config_set_default("crawl:cache", "cache");
 	return 0;
+}
+
+static int
+process_args(int argc, char **argv)
+{
+	int c;
+
+	short_program_name = strrchr(argv[0], '/');
+	if(short_program_name)
+	{
+		short_program_name++;
+	}
+	else
+	{
+		short_program_name = argv[0];
+	}
+	while((c = getopt(argc, argv, "hfdc:")) != -1)
+	{
+		switch(c)
+		{
+		case 'h':
+			usage();
+			exit(EXIT_SUCCESS);
+		case 'f':
+			config_set("crawl:detach", "0");
+			break;
+		case 'd':
+			config_set("log:level", "debug");
+			config_set("log:stderr", "1");
+			config_set("db:debug-queries", "1");
+			config_set("db:debug-errors", "1");
+			config_set("crawl:detach", "0");
+			break;
+		case 'c':
+			config_set("global:configFile", optarg);
+			break;
+		default:
+			usage();
+			return -1;
+		}
+	}
+	argv += optind;
+	argc -= optind;
+	if(argc)
+	{
+		usage();
+		return -1;
+	}
+	return 0;
+}
+
+static void
+usage(void)
+{
+	printf("Usage: %s [OPTIONS]\n"
+		   "\n"
+		   "OPTIONS is one or more of:\n"
+		   "  -h                   Print this notice and exit\n"
+		   "  -f                   Don't detach and run in the background\n"
+		   "  -d                   Enable debug output to standard error\n"
+		   "  -c FILE              Specify path to configuration file\n",
+		   short_program_name);
 }
